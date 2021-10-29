a16606f6e75dc5a89b3dd21705ddcfdc80776d81
PratikGarai <pratikgarai0208@gmail.com>
2021-10-29 14:27:21 +0530

    reformed code

--- dataloader.py
+++ dataloader.py
@@ -5,6 +5,9 @@ import torchvision.transforms as transforms
 from scipy import ndimage
 from glob import glob
 
+from utils.image_utils import get_image_names, get_image_and_mask
+from utils.json_utils import get_classes
+
 
 class segDataset(torch.utils.data.Dataset):
     def __init__(self, root, training, transform=None):
@@ -12,31 +15,13 @@ class segDataset(torch.utils.data.Dataset):
         self.root = root
         self.training = training
         self.transform = transform
-        self.IMG_NAMES = sorted(glob(self.root + '/*/images/*.jpg'))
-        self.BGR_classes = {'Water' : [ 41, 169, 226],
-                            'Land' : [246,  41, 132],
-                            'Road' : [228, 193, 110],
-                            'Building' : [152,  16,  60], 
-                            'Vegetation' : [ 58, 221, 254],
-                            'Unlabeled' : [155, 155, 155]} # in BGR
-
-        self.bin_classes = ['Water', 'Land', 'Road', 'Building', 'Vegetation', 'Unlabeled']
+        self.IMG_NAMES = get_image_names(self.root)
+        self.BGR_classes, self.bin_classes = get_classes(self.root)
 
 
     def __getitem__(self, idx):
         img_path = self.IMG_NAMES[idx]
-        mask_path = img_path.replace('images', 'masks').replace('.jpg', '.png')
-
-        image = cv2.imread(img_path)
-        mask = cv2.imread(mask_path)
-        cls_mask = np.zeros(mask.shape)  
-        cls_mask[mask == self.BGR_classes['Water']] = self.bin_classes.index('Water')
-        cls_mask[mask == self.BGR_classes['Land']] = self.bin_classes.index('Land')
-        cls_mask[mask == self.BGR_classes['Road']] = self.bin_classes.index('Road')
-        cls_mask[mask == self.BGR_classes['Building']] = self.bin_classes.index('Building')
-        cls_mask[mask == self.BGR_classes['Vegetation']] = self.bin_classes.index('Vegetation')
-        cls_mask[mask == self.BGR_classes['Unlabeled']] = self.bin_classes.index('Unlabeled')
-        cls_mask = cls_mask[:,:,0] 
+        image, cls_mask = get_image_and_mask(img_path)
 
         if self.training==True:
             if self.transform:
--- utils/image_utils.py
+++ utils/image_utils.py
@@ -1,8 +1,14 @@
 import cv2 
 import numpy as np
+from glob import glob
+from json_utils import get_classes
 
-def get_image_and_masks(img_path : str, color_dict : dict, classes : list) :
 
+def get_image_names(root : str) :
+    return sorted(glob(root + '/images/*.jpg'))
+
+
+def get_image_and_mask(img_path : str, color_dict : dict, classes : list) :
     mask_path = img_path.replace('images', 'masks')
     image = cv2.imread(img_path)
     mask = cv2.imread(mask_path)
@@ -13,3 +19,31 @@ def get_image_and_masks(img_path : str, color_dict : dict, classes : list) :
     cls_mask = cls_mask[:,:,0] 
 
     return image, cls_mask
+
+
+
+def main() :
+    image_path = get_image_names("../data")[0]
+    mask_path = image_path.replace("images", "masks")
+    mask_path = image_path.replace("images", "masks").replace(".jpg", ".png")
+    mask = cv2.imread(mask_path)
+    # color_dict, class_names = get_classes("../data")
+    # image, mask = get_image_and_mask(image_path, color_dict, class_names)
+    # with open("tmp.txt", "w+") as f : 
+    #     for i in mask :
+    #         for j in i : 
+    #             f.write(f"{j} ")
+    #         f.write("\n")
+    with open("tmp1.txt", "w+") as f : 
+        for i in mask :
+            print(i)
+            for j in i : 
+                f.write(f"{j} ")
+            f.write("\n")
+
+    cv2.imshow("Mask : ", mask)
+    cv2.waitKey()
+    cv2.destroyAllWindows()
+
+if __name__=="__main__" :
+    main()
