22a23e0a93e12d090dabace3efc2038118a384b1
PratikGarai <pratikgarai0208@gmail.com>
2021-11-29 06:30:24 +0530

    updating chennai data getter

--- getChennaiData.sh
+++ getChennaiData.sh
@@ -1,5 +1,5 @@
 pip install -U gdown
 apt-get update && apt-get install unzip
-gdown https://drive.google.com/uc?id=1O6jothR9heO619wovHBy7KBFXAJDSpHP
+gdown https://drive.google.com/uc?id=1_RuRV9SQjFKjtTv8EAhCSFDcsPURbtyV
 mkdir data
 unzip Chennai-Dataset.zip -d ./data
