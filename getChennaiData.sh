c71f4a9e7d1437e4c82661afbbb2072917dbf809
PratikGarai <pratikgarai0208@gmail.com>
2021-11-29 07:20:32 +0530

    new link of chennai data

--- getChennaiData.sh
+++ getChennaiData.sh
@@ -1,5 +1,5 @@
 pip install -U gdown
 apt-get update && apt-get install unzip
-gdown https://drive.google.com/uc?id=1_RuRV9SQjFKjtTv8EAhCSFDcsPURbtyV
+gdown https://drive.google.com/uc?id=1PDiWRox4FAP_T7y2_uBdkDMQBFCeIaV7
 mkdir data
 unzip Chennai-Dataset.zip -d ./data
