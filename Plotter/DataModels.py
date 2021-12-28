909c8047b5444d20a380fa2cafbabdbf1d1568e9
PratikGarai <pratikgarai0208@gmail.com>
2021-12-28 21:34:01 +0530

    added basic parser

--- Plotter/DataModels.py
+++ Plotter/DataModels.py
@@ -1,19 +1,25 @@
 import pickle
+import re
+
+REG = "epoch [0-9]+ - loss \: ([0-9]+\.[0-9]+) \- acc \: ([0-9]+\.[0-9]+)"
 
 class FederatedData : 
+
     def __init__(self) :
         self.n_rounds : int = 0
         self.losses : list = []
         self.accuracies : list = []
         self.round_counts : list = []
     
+
     def save(self, filename : str) :
-        fl = open(filename, "wb")
+        fl = open(filename+".pkl", "wb")
         pickle.dump(self, fl, pickle.HIGHEST_PROTOCOL)
         fl.close()
     
+
     def load(self, filename : str) :
-        fl = open(filename, "rb")
+        fl = open(filename+".pkl", "rb")
         ob : FederatedData = pickle.load(fl, pickle.HIGHEST_PROTOCOL)
         self.n_rounds = ob.n_rounds
         self.losses = ob.losses
@@ -21,10 +27,39 @@ class FederatedData :
         self.round_counts = ob.round_counts
         fl.close()
     
+
     def read_file(self, filename : str, sep : str) :
+        if not sep : 
+            raise Exception("No seperator in Federated mode")
         fl = open(filename, "r")
+        l = None
+        a = None
         for line in fl :
-            # print(line)
-            pass
+            if line.strip()==sep :
+                if self.n_rounds != 0 :
+                    self.losses.append(l)
+                    self.accuracies.append(a)
+                    self.round_counts.append(len(l))
+                self.n_rounds += 1
+                l = []
+                a = []
+            results = re.search(REG, line)
+            if results : 
+                loss, acc = results.groups()
+                l.append(loss)
+                a.append(acc)
+        self.losses.append(l)
+        self.accuracies.append(a)
+        self.round_counts.append(len(l))
         fl.close()
-        print(sep)
+    
+
+    def stringify(self) :
+        s = ""
+        s += f"Rounds          : {self.n_rounds}\n"
+        s += f"Round counts    : {str(self.round_counts)}\n"
+        for i in range(self.n_rounds) :
+            s += f"Acc  : , {str(self.accuracies[i])}\n"
+            s += f"Loss : , {str(self.losses[i])}\n"
+        
+        return s
--- Plotter/LogParser.py
+++ Plotter/LogParser.py
@@ -4,12 +4,13 @@ from DataModels import FederatedData
 def parse(args : ArgumentParser) :
     f = FederatedData()
     f.read_file(args.file, args.sep)
-    return
+    print(f.stringify())
 
 if __name__=="__main__":
     parser = ArgumentParser(description="Convert a federated training log to pkl format")
     parser.add_argument("--file", type=str, required=True, help="Name of the txt file")
     parser.add_argument("--mode", type=str, required=True, choices=["F", "U"], help="Mode of file, U for unified, F for federated")
     parser.add_argument("--sep", type=str, help="Separator of federated rounds in \"quotes\". Required in mode F")
+    parser.add_argument("--name", type=str, required=True, help="Name with which file is to be saved (exclude extensions)")
     args = parser.parse_args()
     parse(args)
