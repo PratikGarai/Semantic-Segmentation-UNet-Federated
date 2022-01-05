3978d5f5b7b2cf5a496071527aa1f42befc02517
PratikGarai <pratikgarai0208@gmail.com>
2022-01-06 01:00:25 +0530

    Plot merger driver graphs set

---
+++ Plotter/MergePlotTable.md
@@ -0,0 +1,56 @@
+# Plots Done
+
+<table>
+    <thead>
+        <th>Sl. No.</th>
+        <th>Description</th>
+    </thead>
+    <tr>
+        <td>1</td>
+        <td>Loss of Round 1 vs Val. Loss of Unified</td>
+    </tr>
+    <tr>
+        <td>2</td>
+        <td>Loss of Round 2 vs Val. Loss of Unified</td>
+    </tr>
+    <tr>
+        <td>3</td>
+        <td>Loss of Round 3 vs Val. Loss of Unified</td>
+    </tr>
+    <tr>
+        <td>4</td>
+        <td>Loss of Round 1 vs Loss of Unified</td>
+    </tr>
+    <tr>
+        <td>5</td>
+        <td>Loss of Round 2 vs Loss of Unified</td>
+    </tr>
+    <tr>
+        <td>6</td>
+        <td>Loss of Round 3 vs Loss of Unified</td>
+    </tr>
+    <tr>
+        <td>7</td>
+        <td>Accuracy of Round 1 vs Val. Accuracy of Unified</td>
+    </tr>
+    <tr>
+        <td>8</td>
+        <td>Accuracy of Round 2 vs Val. Accuracy of Unified</td>
+    </tr>
+    <tr>
+        <td>9</td>
+        <td>Accuracy of Round 3 vs Val. Accuracy of Unified</td>
+    </tr>
+    <tr>
+        <td>10</td>
+        <td>Accuracy of Round 1 vs Accuracy of Unified</td>
+    </tr>
+    <tr>
+        <td>11</td>
+        <td>Accuracy of Round 2 vs Accuracy of Unified</td>
+    </tr>
+    <tr>
+        <td>12</td>
+        <td>Accuracy of Round 3 vs Accuracy of Unified</td>
+    </tr>
+</table>
--- Plotter/PlotterDriver.py
+++ Plotter/PlotterDriver.py
@@ -34,7 +34,7 @@ def plot(args : ArgumentParser) :
     # Plot 2
     # ===============================================
     # Source    : ROUND 2   + Unified
-    # Data      : loss      + val_loss
+    # Data      : loss      + loss
     plot_together({
         "Client 1" : f1.losses[1],
         "Client 2" : f2.losses[1],
@@ -52,6 +52,237 @@ def plot(args : ArgumentParser) :
         "Client 3" : f3.losses[2],
         "Unified " : u.val_losses,
     }, "Val Loss - Round 3")
+
+    # Plot 4
+    # ===============================================
+    # Source    : ROUND 1   + Unified
+    # Data      : loss      + val_loss
+    plot_together({
+        "Client 1" : f1.losses[0],
+        "Client 2" : f2.losses[0],
+        "Client 3" : f3.losses[0],
+        "Unified " : u.losses,
+    }, "Loss - Round 1")
+
+    # Plot 5
+    # ===============================================
+    # Source    : ROUND 2   + Unified
+    # Data      : loss      + loss
+    plot_together({
+        "Client 1" : f1.losses[1],
+        "Client 2" : f2.losses[1],
+        "Client 3" : f3.losses[1],
+        "Unified " : u.losses,
+    }, "Loss - Round 2")
+
+    # Plot 6
+    # ===============================================
+    # Source    : ROUND 3   + Unified
+    # Data      : loss      + val_loss
+    plot_together({
+        "Client 1" : f1.losses[2],
+        "Client 2" : f2.losses[2],
+        "Client 3" : f3.losses[2],
+        "Unified " : u.losses,
+    }, "Loss - Round 3")
+
+    # Plot 7
+    # ===============================================
+    # Source    : ROUND 1   + Unified
+    # Data      : acc       + val_acc
+    plot_together({
+        "Client 1" : f1.accuracies[0],
+        "Client 2" : f2.accuracies[0],
+        "Client 3" : f3.accuracies[0],
+        "Unified " : u.val_accuracies,
+    }, "Val Acc - Round 1")
+
+    # Plot 8
+    # ===============================================
+    # Source    : ROUND 2   + Unified
+    # Data      : acc       + acc
+    plot_together({
+        "Client 1" : f1.accuracies[1],
+        "Client 2" : f2.accuracies[1],
+        "Client 3" : f3.accuracies[1],
+        "Unified " : u.val_accuracies,
+    }, "Val Acc - Round 2")
+
+    # Plot 9
+    # ===============================================
+    # Source    : ROUND 3   + Unified
+    # Data      : acc       + val_acc
+    plot_together({
+        "Client 1" : f1.accuracies[2],
+        "Client 2" : f2.accuracies[2],
+        "Client 3" : f3.accuracies[2],
+        "Unified " : u.val_accuracies,
+    }, "Val Acc - Round 3")
+
+    # Plot 10
+    # ===============================================
+    # Source    : ROUND 1   + Unified
+    # Data      : acc       + val_acc
+    plot_together({
+        "Client 1" : f1.accuracies[0],
+        "Client 2" : f2.accuracies[0],
+        "Client 3" : f3.accuracies[0],
+        "Unified " : u.accuracies,
+    }, "Acc - Round 1")
+
+    # Plot 11
+    # ===============================================
+    # Source    : ROUND 2   + Unified
+    # Data      : acc       + acc
+    plot_together({
+        "Client 1" : f1.accuracies[1],
+        "Client 2" : f2.accuracies[1],
+        "Client 3" : f3.accuracies[1],
+        "Unified " : u.accuracies,
+    }, "Acc - Round 2")
+
+    # Plot 12
+    # ===============================================
+    # Source    : ROUND 3   + Unified
+    # Data      : acc       + val_acc
+    plot_together({
+        "Client 1" : f1.accuracies[2],
+        "Client 2" : f2.accuracies[2],
+        "Client 3" : f3.accuracies[2],
+        "Unified " : u.accuracies,
+    }, "Acc - Round 3")
+    
+    # Plot 13
+    # ===============================================
+    # Source    : Clinet 1 ROUND 1-3    + Unified
+    # Data      : loss                  + val_loss
+    plot_together({
+        "Round 1" : f1.losses[0],
+        "Round 2" : f1.losses[1],
+        "Round 3" : f1.losses[2],
+        "Unified " : u.val_losses,
+    }, "Val Loss - Client 1 vs Unified")
+
+    # Plot 14
+    # ===============================================
+    # Source    : Clinet 2 ROUND 1-3    + Unified
+    # Data      : loss                  + val_loss
+    plot_together({
+        "Round 1" : f2.losses[0],
+        "Round 2" : f2.losses[1],
+        "Round 3" : f2.losses[2],
+        "Unified " : u.val_losses,
+    }, "Val Loss - Client 2 vs Unified")
+
+    # Plot 15
+    # ===============================================
+    # Source    : Clinet 3 ROUND 1-3    + Unified
+    # Data      : loss                  + val_loss
+    plot_together({
+        "Round 1" : f3.losses[0],
+        "Round 2" : f3.losses[1],
+        "Round 3" : f3.losses[2],
+        "Unified " : u.val_losses,
+    }, "Val Loss - Client 3 vs Unified")
+
+    # Plot 16
+    # ===============================================
+    # Source    : Clinet 1 ROUND 1-3    + Unified
+    # Data      : loss                  + loss
+    plot_together({
+        "Round 1" : f1.losses[0],
+        "Round 2" : f1.losses[1],
+        "Round 3" : f1.losses[2],
+        "Unified " : u.losses,
+    }, "Loss - Client 1 vs Unified")
+
+    # Plot 17
+    # ===============================================
+    # Source    : Clinet 2 ROUND 1-3    + Unified
+    # Data      : loss                  + loss
+    plot_together({
+        "Round 1" : f2.losses[0],
+        "Round 2" : f2.losses[1],
+        "Round 3" : f2.losses[2],
+        "Unified " : u.losses,
+    }, "Loss - Client 2 vs Unified")
+
+    # Plot 18
+    # ===============================================
+    # Source    : Clinet 3 ROUND 1-3    + Unified
+    # Data      : loss                  + loss
+    plot_together({
+        "Round 1" : f3.losses[0],
+        "Round 2" : f3.losses[1],
+        "Round 3" : f3.losses[2],
+        "Unified " : u.losses,
+    }, "Loss - Client 3 vs Unified")
+    
+    # Plot 19
+    # ===============================================
+    # Source    : Clinet 1 ROUND 1-3    + Unified
+    # Data      : acc                   + val_acc
+    plot_together({
+        "Round 1" : f1.accuracies[0],
+        "Round 2" : f1.accuracies[1],
+        "Round 3" : f1.accuracies[2],
+        "Unified " : u.val_accuracies,
+    }, "Val Acc - Client 1 vs Unified")
+
+    # Plot 20
+    # ===============================================
+    # Source    : Clinet 2 ROUND 1-3    + Unified
+    # Data      : acc                   + val_acc
+    plot_together({
+        "Round 1" : f2.accuracies[0],
+        "Round 2" : f2.accuracies[1],
+        "Round 3" : f2.accuracies[2],
+        "Unified " : u.val_accuracies,
+    }, "Val Acc - Client 2 vs Unified")
+
+    # Plot 21
+    # ===============================================
+    # Source    : Clinet 3 ROUND 1-3    + Unified
+    # Data      : acc                   + val_acc 
+    plot_together({
+        "Round 1" : f3.accuracies[0],
+        "Round 2" : f3.accuracies[1],
+        "Round 3" : f3.accuracies[2],
+        "Unified " : u.val_accuracies,
+    }, "Val Acc - Client 3 vs Unified")
+
+    # Plot 22
+    # ===============================================
+    # Source    : Clinet 1 ROUND 1-3    + Unified
+    # Data      : acc                   + acc
+    plot_together({
+        "Round 1" : f1.accuracies[0],
+        "Round 2" : f1.accuracies[1],
+        "Round 3" : f1.accuracies[2],
+        "Unified " : u.accuracies,
+    }, "Acc - Client 1 vs Unified")
+
+    # Plot 23
+    # ===============================================
+    # Source    : Clinet 2 ROUND 1-3    + Unified
+    # Data      : acc                   + acc
+    plot_together({
+        "Round 1" : f2.accuracies[0],
+        "Round 2" : f2.accuracies[1],
+        "Round 3" : f2.accuracies[2],
+        "Unified " : u.accuracies,
+    }, "Acc - Client 2 vs Unified")
+
+    # Plot 24
+    # ===============================================
+    # Source    : Clinet 3 ROUND 1-3    + Unified
+    # Data      : acc                   + acc
+    plot_together({
+        "Round 1" : f3.accuracies[0],
+        "Round 2" : f3.accuracies[1],
+        "Round 3" : f3.accuracies[2],
+        "Unified " : u.accuracies,
+    }, "Acc - Client 3 vs Unified")
 
 
 if __name__=="__main__":
